f5b6a94156f950d676cb4ef591ef6cc5b097f99b
maksimn <maksim1989sovunion@yandex.ru>
2014-06-14 02:27:25 +0400

    HTML CSS JavaScript, Day 4

--- reversi.js
+++ reversi.js
@@ -32,36 +32,69 @@ Chip.prototype.setBlack = function () {
     this.side = "black";
 }
 
-window.onload = init;
-
-function init() {
-    var Board = createBoard();
-
-    setInitialPositionOn(Board);
-}
-
-function createBoard() {
-    var boardSize = 8;
-    var Board = new Array(boardSize);
-    for (var i = 0; i < boardSize; i++) {
-        Board[i] = new Array(boardSize);
+function Board() {
+    this.size = 8;
+    this.array = new Array(this.size);
+    for (var i = 0; i < this.array.length; i++) {
+        this.array[i] = new Array(this.size);
     }
-
-    for (var i = 0; i < boardSize; i++) {
-        for (var j = 0; j < boardSize; j++) {
-            Board[i][j] = new Cell(i, j);
+    for (var i = 0; i < this.size; i++) {
+        for (var j = 0; j < this.size; j++) {
+            this.array[i][j] = new Cell(i, j);
         }
     }
-    return Board;
 }
 
-function setInitialPositionOn(Board) {
-    Board[3][3].chip = new Chip();
-    Board[3][3].chip.setWhite();
-    Board[4][4].chip = new Chip();
-    Board[4][4].chip.setWhite();
-    Board[3][4].chip = new Chip();
-    Board[3][4].chip.setBlack();
-    Board[4][3].chip = new Chip();
-    Board[4][3].chip.setBlack();
+Board.prototype.setInitialPositionOfGame = function () {
+    this.array[3][3].chip = new Chip();
+    this.array[3][3].chip.setWhite();
+    this.array[4][4].chip = new Chip();
+    this.array[4][4].chip.setWhite();
+    this.array[3][4].chip = new Chip();
+    this.array[3][4].chip.setBlack();
+    this.array[4][3].chip = new Chip();
+    this.array[4][3].chip.setBlack();
 }
+
+Board.prototype.getCell = function (i, j) {
+    return this.array[i][j];
+}
+
+function View() {
+}
+
+View.prototype.renderModel = function (board) {
+    for (var i = 0; i < board.size; i++) {
+        for (var j = 0; j < board.size; j++) {
+            if (board.getCell(i, j).hasChip()) {
+                var elem_td = document.getElementById(String(i) + String(j));
+                var img = document.createElement("img");
+                img.src = "images/" + board.getCell(i, j).chip.side + ".gif";
+                img.alt = board.getCell(i, j).chip.side;
+                if (elem_td.hasChildNodes()) {
+                    elem_td.removeChild(elem_td.childNodes[0]);
+                }
+                elem_td.appendChild(img);
+            }
+        }
+    }
+}
+
+function init() {
+    var board = new Board();
+    board.setInitialPositionOfGame();
+
+    var view = new View();
+    view.renderModel(board);
+
+    var tds = document.getElementsByTagName("td");
+    function firstTurn(obj) {
+        console.log("clicked element attribute: " + obj.target.getAttribute("id"));
+    }
+
+    for (var i = 0; i < tds.length; i++) {
+        tds[i].onclick = firstTurn;
+    }
+}
+
+window.onload = init;
